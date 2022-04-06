d8d3411047ac809dab883ff0337551d3eb8921ef
CARLOS ANDRES QUEL REDROBAN <carlos.quel@epn.edu.ec>
2022-04-06 01:04:22 -0500

    Auth cookie

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,19 @@
 		<nav class="navbar navbar-expand-lg navbar-light bg-primary bg-gradient">
 			<div class="container-fluid">
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<ul class="navbar-nav ml-auto" v-if="getUserProfile.id === 0">
 						<li class="nav-item">
 							<router-link to="/login" class="nav-link">Login</router-link>
+						</li>
+					</ul>
+					<ul class="navbar-nav ml-auto" v-else>
+						<li class="nav-item">
+							<router-link to="/dashboard" class="nav-link">Dashboard</router-link>
+						</li>
+						<li class="nav-item">
+							<button type="button" class="btn btn-link text-decoration-none text-body" @click="logout()">
+								Logout
+							</button>
 						</li>
 					</ul>
 				</div>
@@ -17,8 +27,37 @@
 </template>
 
 <script>
+import { mapGetters, mapActions, mapMutations } from "vuex";
 export default {
-  name: "App",
+	name: "App",
+	computed: {
+		...mapGetters('auth', {
+			getUserProfile: "getUserProfile",
+			getLogout: "getLogout",
+		})
+	},
+	methods: {
+		...mapActions("auth", {
+			userLogout: "userLogout",
+		}),
+		...mapMutations("auth", {
+			setLogout: "setLogout",
+			setUserProfile: "setUserProfile",
+		}),
+		async logout() {
+			await this.userLogout();
+			if (this.getLogout) {
+				const resetUser = {
+					id: 0,
+					identity: "",
+				};
+				this.setUserProfile(resetUser);
+				this.setLogout(false);
+				this.$router.replace('/login'); // Go back login page
+				// this.$router.go();   // Refresh page
+			}
+		},
+	},
 };
 </script>
 
